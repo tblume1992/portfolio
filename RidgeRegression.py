69baabfd64a41c1dcd99d75b9527f539e0a3972e
tblume1992 <t-blume@hotmail.com>
2018-06-01 18:08:30 -0400

    Add files via upload

---
+++ RidgeRegression.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  1 17:43:42 2018
+
+@author: tyler
+"""
+from numpy.linalg import inv
+import pandas as pd
+import numpy as np
+
+def Ridge(y, X, alpha, intercept = True):
+    if intercept is True:
+        intercept = np.ones(5)
+    I = np.eye(X.shape[1])
+    ridge = inv(np.dot(X.T,X) + alpha*I)
+    esti = np.dot(X.T,y)
+    coefficients = pd.DataFrame(np.round(np.dot(ridge,esti),3))
+    coefficients.columns = ['Coefficients']
+    fitted = np.dot(X,coefficients)
+    return coefficients, fitted
