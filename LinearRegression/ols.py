0d46a1953436587e3fb60813228f70b1d9adafa3
tblume1992 <t-blume@hotmail.com>
2018-06-26 10:48:13 -0400

    Update ols.py

--- LinearRegression/ols.py
+++ LinearRegression/ols.py
@@ -8,21 +8,21 @@ Created on Sat Jun  2 08:56:36 2018
 from numpy.linalg import inv
 import pandas as pd
 import numpy as np
-
 class OLS():
-    def fit(y, X, intercept = True):
-        OLS.intercept = intercept
-        if intercept is True:
-            constant = np.ones((X.shape[0],1))
-            X = np.append(X,constant,1)
+    def __init__(self, intercept = True):
+        self.intercept = intercept
+    def add_constant(self, X):
+        constant = np.ones((X.shape[0],1))
+        return np.append(X,constant,1)
+    def fit(self,y, X):
+        if self.intercept:
+            X = self.add_constant(X)
         ols = inv(np.dot(X.T,X))
         esti = np.dot(X.T,y)
         coefficients = pd.DataFrame(np.round(np.dot(ols,esti),3))
-        coefficients.columns = ['Coefficients']
-        OLS.coefficients_ = coefficients
-        OLS.fitted_ = np.dot(X,coefficients)
-    def predict(X_test):
-        if OLS.intercept is True:
-            constant = np.ones((X_test.shape[0],1))
-            X_test = np.append(X_test,constant,1)
-        OLS.predictions_ = np.dot(X_test,OLS.coefficients_)
+        self.coefficients_ = coefficients
+        self.fitted_ = np.dot(X,coefficients)
+    def predict(self,X_test):
+        if self.intercept:
+            X_test = self.add_constant(X_test)
+        self.predictions_ = np.dot(X_test,self.coefficients_)
