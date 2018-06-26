27cd21e1e8bec9b07d5ed4d5aac2f94e8595f6d8
tblume1992 <t-blume@hotmail.com>
2018-06-26 11:39:00 -0400

    Update WLS.py

--- LinearRegression/WLS.py
+++ LinearRegression/WLS.py
@@ -12,40 +12,46 @@ import numpy as np
 from scipy import stats
 
 class WLS():
-    def fit(y, X, intercept = True):
-        if intercept is True:
-            constant = np.ones((X.shape[0],1))
-            X = np.append(X,constant,1)
-        wls = inv(np.dot(X.T,X))
+    def __init__(self, intercept = True):
+        self.intercept = intercept
+    def add_constant(self, X):
+        constant = np.ones((X.shape[0],1))
+        return np.append(X,constant,1)
+    def ols_fit(self,y, X):
+        if self.intercept:
+            X = self.add_constant(X)
+        ols = inv(np.dot(X.T,X))
         esti = np.dot(X.T,y)
-        coefficients = pd.DataFrame(np.round(np.dot(wls,esti),3))
+        coefficients = pd.DataFrame(np.round(np.dot(ols,esti),3))
         coefficients.columns = ['Coefficients']
-        WLS.pre_wls = coefficients
-        fitted_ = np.dot(X,coefficients)
-        WLS.residuals_ = (y-fitted_)
-        WLS.omega_ = np.diag(np.diag(np.cov(WLS.residuals_)))
-        WLS.inv_omega = np.asarray(inv(WLS.omega_))
-        
-        wls = np.dot(X.T,WLS.inv_omega)
+        return np.dot(X,coefficients)
+    def get_weight(self):
+        omega = np.diag(np.diag(np.cov(self.residuals_)))
+        return np.asarray(inv(omega))
+    def fit(self, y, X):
+        self.residuals_ = (y-self.ols_fit(y,X))  
+        if self.intercept:    
+            X = self.add_constant(X)
+        wls = np.dot(X.T,self.get_weight())
         wls = inv(np.dot(wls,X))
-        esti = np.dot(X.T,WLS.inv_omega)
+        esti = np.dot(X.T,self.get_weight())
         esti = np.dot(esti,y)
-        WLS.coefficients =pd.DataFrame(np.round(np.dot(wls,esti),3))
+        self.coefficients_ =pd.DataFrame(np.round(np.dot(wls,esti),3))
         variance = inv(np.dot(X.T,X))
-        variance2 = np.dot(X.T,WLS.omega_)
+        variance2 = np.dot(X.T,self.get_weight())
         variance2 = np.dot(variance2,X)
         variance_final = np.dot(variance,variance2)
-        WLS.variance_final = np.diag(np.dot(variance_final,variance))
-        WLS.se_ = np.sqrt(WLS.variance_final)/np.sqrt(len(y))
-        output = pd.DataFrame(WLS.coefficients)
+        self.variance_final = np.diag(np.dot(variance_final,variance))
+        self.se_ = np.sqrt(self.variance_final)/np.sqrt(len(y))
+        output = pd.DataFrame(self.coefficients_)
         output.columns = ['Coefficients']
-        output['Standard Error'] = WLS.se_
+        output['Standard Error'] = self.se_
         output['T-Stat'] = output['Coefficients']/output['Standard Error']
         output['p-values'] = stats.t.sf(np.abs(output['T-Stat']), len(y)-1)*2.0
         output = output.round(decimals = 3)
         print("****************************WLS Results****************************")
         print(output)
-    def predict(X_test):
-        constant = np.ones((X_test.shape[0],1))
-        X_test = np.append(X_test,constant,1)
-        WLS.predictions_ = np.dot(X_test,WLS.coefficients_)
+    def predict(self, X_test):
+        if self.intercept:
+            X_test = self.add_constant(X_test)
+        return np.dot(X_test,self.coefficients_)
